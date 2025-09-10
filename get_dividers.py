628200c2133bafd9a70ba16de5ab6f03a7baf140
ghh-jb <138157456+ghh-jb@users.noreply.github.com>
2025-09-10 15:42:02 +0300

    Fix 1 and multiple same dividers
    for ex: 100 = 10*10, was returning 10 twice in the ret list
    Now returns 1 and itself in the list, if 1 was passed as arg - return [1]

--- get_dividers.py
+++ get_dividers.py
@@ -1,8 +1,21 @@
+# def get_dividers(num:int) -> list:
+#     # num = abs(num)
+#     dividers = [num]
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             dividers.append(i)
+#             dividers.append(num // i)
+#     return dividers
+# ^^^ V1.0
+
 def get_dividers(num:int) -> list:
     # num = abs(num)
-    dividers = [num]
+    if num == 1:
+        return [1]
+    dividers = [1, num]
     for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             dividers.append(i)
             dividers.append(num // i)
-    return dividers
+    return list(set(dividers))
+# ^^^ V1.1
