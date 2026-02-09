31acbd58aa4ab0175771801211794727b2c9a7a8
ghh-jb <138157456+ghh-jb@users.noreply.github.com>
2026-02-09 13:10:28 +0300

    another port of python code but for C
    recursive find of greatest common divider

---
+++ cgeatest_common_divider.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+long long grand_central_dispatcher(long long a, long long b, long long *x, long long *y) {
+    if (a == 0) {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+    long long x1, y1;
+    long long d = grand_central_dispatcher(b % a, a, &x1, &y1);
+    *x = y1 - (b / a) * x1;
+    *y = x1;
+    return d;
+}
